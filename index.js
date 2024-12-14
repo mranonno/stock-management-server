727caafd0eb7b5c028064bc61e502ac0c55bfdc1
shuvajitmaitra <shuvajitmaitra@gmail.com>
2024-12-14 09:56:45 +0600

    cros update

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -122,9 +126,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
